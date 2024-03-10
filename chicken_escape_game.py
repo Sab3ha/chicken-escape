24fa8ddef403b6ed6df36dd0917797a92f5d3643
Sabeha Khan <sabehak321@gmail.com>
2024-03-10 01:13:43 -0500

    Display background image

--- chicken_escape_game.py
+++ chicken_escape_game.py
@@ -1,1 +1,13 @@
 import pygame
+
+screen_size = [360, 600]
+screen = pygame.display.set_mode(screen_size)
+
+#load the background image
+background = pygame.image.load('chicken-escape/background.png')
+
+keep_alive = True
+while keep_alive:
+    #display the background image
+    screen.blit(background, [0,0])
+    pygame.display.update()
