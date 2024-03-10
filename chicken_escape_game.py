7743df7e9cf2666df69465eb12fd945546440ada
Sabeha Khan <sabehak321@gmail.com>
2024-03-10 01:18:06 -0500

    Display chicken and user images

--- chicken_escape_game.py
+++ chicken_escape_game.py
@@ -3,11 +3,16 @@ import pygame
 screen_size = [360, 600]
 screen = pygame.display.set_mode(screen_size)
 
-#load the background image
+#load the images
 background = pygame.image.load('chicken-escape/background.png')
+user = pygame.image.load('chicken-escape/user.png')
+chicken = pygame.image.load('chicken-escape/chicken.png')
 
 keep_alive = True
+#game loop
 while keep_alive:
-    #display the background image
+    #display the images
     screen.blit(background, [0,0])
+    screen.blit(user, [0,0])
+    screen.blit(chicken, [0,0])
     pygame.display.update()
