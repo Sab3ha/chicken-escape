d3d695e0b001967c7921eecccf1790b6320a768a
Sabeha Khan <sabehak321@gmail.com>
2024-03-10 01:24:58 -0500

    Add y-coordinate for chicken

--- chicken_escape_game.py
+++ chicken_escape_game.py
@@ -7,12 +7,14 @@ screen = pygame.display.set_mode(screen_size)
 background = pygame.image.load('chicken-escape/background.png')
 user = pygame.image.load('chicken-escape/user.png')
 chicken = pygame.image.load('chicken-escape/chicken.png')
+chicken_y = 0
 
 keep_alive = True
 #game loop
 while keep_alive:
+    chicken_y = chicken_y + 5
     #display the images
     screen.blit(background, [0,0])
     screen.blit(user, [150,520])
-    screen.blit(chicken, [0,0])
+    screen.blit(chicken, [0,chicken_y])
     pygame.display.update()
