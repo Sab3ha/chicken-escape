da4312c553eb603c5634cf485374f8362a5620af
Sabeha Khan <sabehak321@gmail.com>
2024-03-10 01:50:37 -0500

    Add right and left key movement

--- chicken_escape_game.py
+++ chicken_escape_game.py
@@ -14,6 +14,7 @@ def random_offset():
     return -1*random.randint(100, 1500)
 
 chicken_y = [random_offset(), random_offset(), random_offset()]
+user_x = 150
 
 def update_chicken_position(i):
     if chicken_y[i] > 600:
@@ -25,6 +26,13 @@ keep_alive = True
 clock = pygame.time.Clock()
 #game loop
 while keep_alive:
+    pygame.event.get()
+    keys =  pygame.key.get_pressed()
+    if keys[pygame.K_RIGHT]:
+        user_x = user_x + 10
+    elif keys[pygame.K_LEFT]:
+        user_x = user_x - 10
+    
     update_chicken_position(0)
     update_chicken_position(1)
     update_chicken_position(2)
@@ -32,7 +40,7 @@ while keep_alive:
     
     #display the images
     screen.blit(background, [0,0])
-    screen.blit(user, [150,520])
+    screen.blit(user, [user_x,520])
     screen.blit(chicken, [0,chicken_y[0]])
     screen.blit(chicken, [150,chicken_y[1]])
     screen.blit(chicken, [280,chicken_y[2]])
